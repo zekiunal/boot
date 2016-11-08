fda32a0197e1d31acaaa57588161fc6ee76d1e4a
Zeki Unal <zekiunal@gmail.com>
2016-11-08 12:15:52 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -76,16 +76,71 @@ echo PROVIDER="$PROVIDER"                >> /etc/docker/environments
 echo SD_BOOT="$SD_BOOT"                  >> /etc/docker/environments
 echo SWARM_MASTER_IP="$SWARM_MASTER_IP"  >> /etc/docker/environments
 
-docker network create --driver overlay syslog
-docker service create --with-registry-auth --name="syslog" --replicas 6  --limit-memory="16mb" \
-    --network syslog \
-    -e SERVICE_514_NAME="syslog" -e SERVICE_514_TAGS="service" \
-    -e CONSUL_ADDRESS=$(hostname --i) \
-    registry.monapi.com:5000/monapi/syslog
-    
-docker network create --driver overlay proxy
-
 docker pull registry.monapi.com:5000/monapi/fpm:monapi-5.6.27
 docker pull registry.monapi.com:5000/monapi/alpine:3.4
 groupadd -r docker
 usermod -a -G docker newrelic
+
+docker network create --driver overlay proxy
+
+# Log
+############################################################################################################################################
+docker network create --driver overlay syslog
+docker service create --with-registry-auth --name="syslog" --mode global --reserve-memory 64m \
+    --container-label com.monitoring.group=logging \
+    --network syslog \
+    -e SERVICE_514_NAME="syslog" -e SERVICE_514_TAGS="service" \
+    -e CONSUL_ADDRESS=$(hostname --i) \
+    registry.monapi.com:5000/monapi/syslog
+
+docker service create --name node-exporter --mode global --network proxy --reserve-memory 64m \
+--container-label com.monitoring.group=logging \
+--mount "type=bind,source=/proc,target=/host/proc" \
+--mount "type=bind,source=/sys,target=/host/sys" \
+--mount "type=bind,source=/,target=/rootfs" \
+--mount "type=bind,source=/etc/hostname,target=/etc/host_hostname" \
+-e HOST_HOSTNAME=/etc/host_hostname \
+basi/node-exporter:v0.1.1 \
+-collector.procfs /host/proc \
+-collector.sysfs /host/proc \
+-collector.filesystem.ignored-mount-points "^/(sys|proc|dev|host|etc)($|/)" \
+-collector.textfile.directory /etc/node-exporter/ \
+-collectors.enabled="conntrack,diskstats,entropy,filefd,filesystem,loadavg,mdadm,meminfo,netdev,netstat,stat,textfile,time,vmstat,ipvs"
+
+docker service create --name cadvisor --reserve-memory 64m \
+--container-label com.monitoring.group=logging \
+--mode global \
+--network proxy \
+--mount "type=bind,source=/,target=/rootfs" \
+--mount "type=bind,source=/var/run,target=/var/run" \
+--mount "type=bind,source=/sys,target=/sys" \
+--mount "type=bind,source=/var/lib/docker,target=/var/lib/docker" \
+google/cadvisor:v0.24.1
+
+sudo mkdir -p /docker/prometheus/data
+sudo mkdir -p /docker/prometheus/config
+sudo wget  -O /docker/prometheus/config/prometheus.yml https://raw.githubusercontent.com/vfarcic/cloud-provisioning/master/conf/prometheus.yml
+
+docker service create --reserve-memory 64m \
+--container-label com.monitoring.group=logging \
+--repicas 1 \
+--constraint 'node.role == manager' --constraint 'engine.labels.role == master' \
+--name prometheus \
+--network proxy \
+-p 9090:9090 \
+--constraint 'node.role == manager' --constraint 'engine.labels.role == master' \
+--mount "type=bind,source=/docker/prometheus/config/prometheus.yml,target=/etc/prometheus/prometheus.yml" \
+--mount "type=bind,source=/docker/prometheus/data,target=/prometheus" \
+prom/prometheus:v1.2.1
+
+docker service create --reserve-memory 64m \
+--container-label com.monitoring.group=logging \
+--name grafana \
+--network proxy \
+-p 3000:3000 \
+grafana/grafana:3.1.1
+
+docker service create --with-registry-auth --name elasticsearch --network proxy --container-label com.monitoring.group=logging -p 9200:9200 --reserve-memory 500m  elasticsearch:2.4
+docker service create --with-registry-auth --name logstash      --network proxy --container-label com.monitoring.group=logging -e LOGSPOUT=ignore --reserve-memory 500m  registry.monapi.com:5000/monapi/logstash:service2
+docker service create --with-registry-auth --name logspout      --network proxy --container-label com.monitoring.group=logging --mode global --mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" -e SYSLOG_FORMAT=rfc3164 gliderlabs/logspout syslog://logstash:51415
+
