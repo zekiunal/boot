f84916727be3f6c116ff1a8c717d1d43cc0179fd
Zeki Unal <zekiunal@gmail.com>
2016-12-29 17:20:35 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -68,8 +68,7 @@ echo PROVIDER="$PROVIDER"                >> /etc/docker/environments
 echo SD_BOOT="$SD_BOOT"                  >> /etc/docker/environments
 echo SWARM_MASTER_IP="$SWARM_MASTER_IP"  >> /etc/docker/environments
 
-docker pull registry.monapi.com:5000/monapi/fpm:monapi-5.6.27
-docker pull registry.monapi.com:5000/monapi/fpm:nginx
+docker pull registry.monapi.com:5000/monapi/fpm:monapi-5.6.29
 docker pull registry.monapi.com:5000/monapi/alpine:3.4
 
 # @todo prometheus storage volume
