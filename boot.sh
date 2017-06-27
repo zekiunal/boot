50a586f1e79d1226cc13f6e4f2a0eb77d096586c
Zeki Unal <zekiunal@gmail.com>
2017-06-27 14:39:57 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -14,13 +14,24 @@ SERVICE_TYPE=$(echo ${EXIST_NODE} | jq '.Tags[] | select(.Key == "Type")'  | jq 
 ROLE=$(echo ${EXIST_NODE} | jq '.Tags[] | select(.Key == "Role")'  | jq  -r ".Value" )
 ENV=$(echo ${EXIST_NODE} | jq '.Tags[] | select(.Key == "Env")'  | jq  -r ".Value" )
 
+while [[ -z $VAULT_SERVER ]]; do
+   echo 'Waiting for swarm master run ...'
+   VAULT_SERVER=$(docker run -i --rm --name aws -v /home/core/.aws:/root/.aws cgswong/aws:aws ec2 describe-instances --filters Name=instance-state-name,Values=running Name=tag:Type,Values=ca-server Name=tag:Role,Values=master Name=tag:Stack,Values=${STACK} | jq -r ".Reservations[].Instances[]")
+   sleep 1
+done
+
+VAULT_SERVER_IP=$(echo ${VAULT_SERVER} | jq '."PrivateIpAddress"')
+VAULT_SERVER_IP=${VAULT_SERVER_IP#\"}
+VAULT_SERVER_IP=${VAULT_SERVER_IP%$suffix}
+VAULT_SERVER_IP=${VAULT_SERVER_IP/\"/}
+
 if [ "$SERVICE_TYPE" == "swarm-manager" ] && [ "$ROLE" == "master" ]; then
    SWARM_MASTER_IP=${PR_IPV4}
    docker swarm init --advertise-addr ${PR_IPV4}
    docker run -i --rm --name aws -v /home/core/.aws:/root/.aws cgswong/aws:aws ec2 create-tags --resources ${INSTANCE_ID} --tags Key=ManagerToken,Value=$(docker swarm join-token -q manager)
    docker run -i --rm --name aws -v /home/core/.aws:/root/.aws cgswong/aws:aws ec2 create-tags --resources ${INSTANCE_ID} --tags Key=WorkerToken,Value=$(docker swarm join-token -q worker)
    SD_BOOT="-server -advertise ${PR_IPV4} -advertise-wan ${PUBLIC_IPV4} -bootstrap"
-    
+
 elif [ "$SERVICE_TYPE" == "swarm-manager" ] && [ "$ROLE" == "slave" ]; then
    while [[ -z $SWARM_MASTER_NODE ]]; do
        echo 'Waiting for swarm master run ...'
@@ -68,8 +79,9 @@ echo REGION="'${REGION}'"                    >> /etc/docker/environments
 echo PROVIDER="'${PROVIDER}'"                >> /etc/docker/environments
 echo SD_BOOT="'${SD_BOOT}'"                  >> /etc/docker/environments
 echo SWARM_MASTER_IP="'${SWARM_MASTER_IP}'"  >> /etc/docker/environments
+echo VAULT_SERVER_IP="'${VAULT_SERVER_IP}'"  >> /etc/docker/environments
 
-docker pull registry.monapi.com:5000/monapi/fpm:monapi-5.6.29
+docker pull registry.monapi.com:5000/monapi/fpm:monapi-5.6.30
 docker pull registry.monapi.com:5000/monapi/alpine:3.4
 
 # @todo prometheus storage volume
@@ -81,5 +93,5 @@ sudo cat << EOF > /tmp/${ENV}.json
 {"domain": "${STACK}", "ip": "${SWARM_MASTER_IP}"}
 EOF
 
-sudo docker run -i --rm --name aws -v /tmp:/tmp -v /home/core/.aws:/root/.aws cgswong/aws:aws s3 cp /tmp/${ENV}.json s3://bucket.ci.monapi.com/${ENV}.json 
+sudo docker run -i --rm --name aws -v /tmp:/tmp -v /home/core/.aws:/root/.aws cgswong/aws:aws s3 cp /tmp/${ENV}.json s3://bucket.ci.monapi.com/${ENV}.json
 #cd / && sudo curl -L git.io/scope -o /scope && sudo chmod a+x /scope && sudo chown core:core /scope  && ./scope launch --service-token=c84ffqfkjk37nj564dg6jxicd3egog85
